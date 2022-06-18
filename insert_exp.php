6cb9b45733dbc3f41bdc849b78c73a45c0799d13
Tom-Sarfo <74250845+Tom-Sarfo@users.noreply.github.com>
2022-06-17 22:17:31 -0600

    Update insert_exp.php

--- insert_exp.php
+++ insert_exp.php
@@ -1,5 +1,5 @@
 <?php  
- $connect = mysqli_connect("localhost", "tomusbir_sarfo", "snpb]aO_hA)9", "tomusbir_account");   
+ require_once '../db.php';  
  $sql = "INSERT INTO retail_orders(exp_description, quantity, price, exp_date, exp_id, exp_status) VALUES('".$_POST["exp_description"]."', '".$_POST["quantity"]."', '".$_POST["price"]."', '".$_POST["exp_date"]."', '".$_POST["exp_id"]."', '".$_POST["exp_status"]."')";  
  if(mysqli_query($connect, $sql))  
  {  
